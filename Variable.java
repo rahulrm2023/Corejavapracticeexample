0530a55074d36784bafeff8c2bb0be4a0f8cc06c
rahulrm2023 <metkarir9@gmail.com>
2026-03-02 22:42:47 +0530

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,120 @@
+// five class of variable 
+//class no 1 bank 
+package variable;
+
+public class Bank {
+	public static void main(String[] args) {
+
+		String accountHolderName = "Ram Shyam Mane ";
+		long ifcsNo=455565255;
+		long accountNo = 515455455l;
+		long mobileno = 415456555526l;
+		String panNo= "GHKJ123l";
+		String adderss = "sangola";
+		String mail="ramrfy12@gmail.com";
+		double accountBalance=12356.90;
+		System.out.println(accountHolderName);
+		System.out.println(ifcsNo);
+		System.out.println(accountNo);
+		System.out.println(mobileno);
+		System.out.println(panNo);
+		System.out.println(adderss);
+		System.out.println(mail);
+		System.out.println(accountBalance);
+
+
+	}
+
+}
+// class no 2 Employee
+package variable;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		String empname="Shyam Sathe";
+		String address="Ram Peth , city colony , solapur";
+		long adharNo=777474774845l;
+		int eId=101;
+		String departname="Manufactre";
+		int shift=1;
+		double salary=100000.55;
+		String role = "Manager";
+		System.out.print("name = "+empname);
+		System.out.print("address = "+address);
+		System.out.print("eId = "+eId);
+		System.out.print("department = "+departname);
+		System.out.print("shift = "+shift);
+		System.out.print("salary = "+salary);
+		System.out.print("role = "+role);
+
+	}
+
+}
+// class no 3 Hospital 
+package variable;
+
+public class Hospital {
+
+	public static void main(String[] args) {
+		int hosId = 2121;
+		String hname = "goverment hospital";
+		String pName = "Ram Sathe";
+		int noOfBed = 20;
+		String departName = "ardiology";
+		int bill = 2000;
+
+		System.out.println(hname);
+		System.out.println(pName);
+		System.out.println(noOfBed);
+		System.out.println(departName);
+		System.out.println(bill);
+
+	}
+}
+// class no 4 Library
+package variable;
+
+public class Library {
+	public static void main(String[] args) {
+		String stuName = "ram";
+		int noOfBook = 2000;
+		int noOfSection=4;
+		int fee=3000;
+		float inTime=9.12f;
+		int typeOfBook=4;
+		System.out.println(stuName);
+		System.out.println(noOfBook);
+		System.out.println(noOfSection);
+		System.out.println(fee);
+		System.out.println(inTime);
+		System.out.println(typeOfBook);
+
+	}
+
+}
+// class no 5 Student 
+package variable;
+
+public class Student {
+	public static void main(String[] args) {
+		String name= "Ram shelke";
+		int sId= 112;
+		long mobileNo=8010353696l;
+		String department= "CSE";
+		long prnNo=2267561242119l;
+		String currentYear="Last year";
+		String course="Java Fullstack ";
+		System.out.print("name ="+ name );
+		System.out.print("sId ="+ sId);
+		System.out.print("mobileNo ="  +mobileNo);
+		System.out.print("departname ="+  department);
+		System.out.print("prnNo ="+  prnNo);
+		System.out.print("currentYear ="+ currentYear);
+		System.out.print("course ="+ course);
+		
+
+	}
+
+}
+
