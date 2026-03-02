d7ed502c5966d27aeef611e48dd511617f0c25e0
rahulrm2023 <metkarir9@gmail.com>
2026-03-02 14:42:27 +0530

    Update day1.java

--- day1.java
+++ day1.java
@@ -3,8 +3,28 @@ package com.tka;
 public class day1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int i =90;
+		i++; // 90 & 91
+		int j = i + 9; // 100
+		System.out.println(i++); // 91
+		--j; // 99
+		System.out.println(j * i); // 99
+		i++; // 91 & 92
+		--i; // 91
+		System.out.println(--i); // 91
+		++i; // 92
+		System.out.println(j + 3); // 102
+		j--; // 102 & 101
+		++j; // 102
+		System.out.println(j + 7); // 106
+		j--; // 99 & 98
+		i++; // 92 & 93
+		--i; // 92
+		System.out.println(i + j); // 190
+		System.out.println(i);// 92
+		System.out.println(j);// 98
 
 	}
 
 }
+
