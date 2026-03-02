e741f72bfb2697848fd990431550292f9395abfb
rahulrm2023 <metkarir9@gmail.com>
2026-03-02 22:30:05 +0530

    Create Arithamaticoperator.java

---
+++ Arithamaticoperator.java
@@ -0,0 +1,25 @@
+package operator;
+
+public class Arithamaticope {
+
+	public static void main(String[] args) {
+		int a = 10, b = 17;
+		int c = (a + 30);
+		int d = c / a;
+		int e = a % b;
+		int f = c % a;
+		int h = f * d;
+		int i = c-b-a;
+		System.out.println("Result is following ");
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(h);
+		System.out.println(i);
+
+
+
+	}
+
+}
