f14baf8ef376406ddddf502c14913014883bb39c
rahulrm2023 <metkarir9@gmail.com>
2026-03-02 22:21:41 +0530

    Create Defaultsize.java

---
+++ Defaultsize.java
@@ -0,0 +1,147 @@
+//class 1 Bank
+package Defaultsize;
+
+public class Bank {
+	static String accountHolderName="Ram Shyam Mane ";
+	static long ifcsNo;
+	static long accountNo=515455455l;
+	static long mobileno=415456555526l;
+	static String panNo;
+	static String adderss="sangola";
+	static String mail;
+	static double accountBalance;
+	public static void main(String[] args) {
+		System.out.println(accountHolderName);
+		System.out.println(ifcsNo);
+		System.out.println(accountNo);
+		System.out.println(mobileno);
+		System.out.println(panNo);
+		System.out.println(adderss);
+		System.out.println(mail);
+		System.out.println(accountBalance);
+				
+}
+
+}
+
+//class no 2 
+package Defaultsize;
+
+public class Employee {
+	static String empname = "Shyam Sathe";
+	static String address = "Ram Peth , city colony , solapur";
+	static long adharNo = 777474774845l;
+	static int eId = 101;
+	static String departname = "Manufactre";
+	static int shift = 1;
+	static double salary = 100000.55;
+	static String role = "Manager";
+
+	public static void main(String[] args) {
+		System.out.print("name = " + empname);
+		System.out.print("address = " + address);
+		System.out.print("eId = " + eId);
+		System.out.print("department = " + departname);
+		System.out.print("shift = " + shift);
+		System.out.print("salary = " + salary);
+		System.out.print("role = " + role);
+
+	}
+
+}
+//class no 3
+package Defaultsize;
+
+public class Hospital {
+	static int hosId=2121;
+	static String hname;
+	static int pName;
+	static int noOfBed=20;
+	static String departName= "ardiology";
+	static int bill=2000;
+	
+	public static void main(String[] args) {
+		System.out.println(hname);
+		System.out.println(pName);
+		System.out.println(noOfBed);
+		System.out.println(departName);
+		System.out.println(bill);
+		
+
+	}
+
+}
+// class no 3 Hospital 
+package Defaultsize;
+
+public class Hospital {
+	static int hosId=2121;
+	static String hname;
+	static int pName;
+	static int noOfBed=20;
+	static String departName= "ardiology";
+	static int bill=2000;
+	
+	public static void main(String[] args) {
+		System.out.println(hname);
+		System.out.println(pName);
+		System.out.println(noOfBed);
+		System.out.println(departName);
+		System.out.println(bill);
+		
+
+	}
+
+}
+//Class no 4 Library
+package Defaultsize;
+
+public class library {
+	static String stuName="ram";
+	static int noOfBook=2000;
+	static int noOfSection ;
+	static int fee;
+	static float inTime;
+	static int typeOfBook;
+
+	public static void main(String[] args) {
+		System.out.println(stuName);
+		System.out.println(noOfBook);
+		System.out.println(noOfSection);
+		System.out.println(fee);
+		System.out.println(inTime);
+		System.out.println(typeOfBook);
+
+}
+}
+// Class no 5 Student 
+package Defaultsize;
+
+public class Student {
+	static String name = "Ram shelke";
+	static int sId = 112;
+	static long mobileNo ;
+	static String department = "CSE";
+	static long prnNo ;
+	static String currentYear = "Last year";
+	static String course = "Java Fullstack ";
+
+	public static void main(String[] args) {
+		String name = "Ram shelke";
+		int sId = 112;
+		long mobileNo = 8010353696l;
+		String department = "CSE";
+		long prnNo = 2267561242119l;
+		String currentYear = "Last year";
+		String course = "Java Fullstack ";
+		System.out.print("name =" + name);
+		System.out.print("sId =" + sId);
+		System.out.print("mobileNo =" + mobileNo);
+		System.out.print("departname =" + department);
+		System.out.print("prnNo =" + prnNo);
+		System.out.print("currentYear =" + currentYear);
+		System.out.print("course =" + course);
+
+	}
+
+}
