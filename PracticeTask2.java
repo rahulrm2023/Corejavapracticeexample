69eb5dbf18192abf80293263e3adce5180eaad83
rahulrm2023 <metkarir9@gmail.com>
2026-03-04 14:58:04 +0530

    Create PracticeTask2.java

---
+++ PracticeTask2.java
@@ -0,0 +1,30 @@
+package assignmentoperator;
+
+public class PracticeTask2 {
+
+	public static void main(String[] args) {
+
+		int i = 10;
+		i+=4; // 14
+		i*=2; // 28
+		System.out.println(i++); //28 //29
+		i/=2; //14
+		System.out.println(i % 2); // 0
+		++i; // 15
+		System.out.println(i += 5); //20
+		i++; // 21
+		int d = i + 10; // 32
+		System.out.println(i++); // 21// 22
+		--i; // 21
+		i++; //22
+		i++; //23
+		System.out.println(i+5); //28
+		i %= 2; // 1
+		System.out.println(++i); // 1//2
+		++i; // 3
+		System.out.println(i + 10); // 13
+		System.out.println(i);//3
+
+	}
+
+}
