7797d1c712c665593da14203434af5b7f6e35932
Rahul Metkari <metkarir9@gmail.com>
2026-03-10 23:40:23 +0530

    Create Switchcondition.java

---
+++ Switchcondition.java
@@ -0,0 +1,244 @@
+// Accept a category type (Domestic, Commercial, Industrial) and print the electricity rate per unit using a switch case.
+package Switch.txt;
+
+public class LightBill {
+
+	public static void main(String[] args) {
+		int unit=56;
+		int rate ;
+		float bill;
+		String use ="Commerical";
+		switch(use) {
+		case "Domestic":
+			rate =7;
+		bill=unit*rate;
+		System.out.println("Bill is = "+bill);
+		break;
+		case "Commerical":
+			rate =10;
+		bill=unit*rate;
+		System.out.println("Bill is = "+bill);
+		break;
+		case "Industrial":
+			rate =12;
+		bill=unit*rate;
+		System.out.println("Bill is = "+bill);
+		break;
+		default:
+			System.out.println("Enter valid input ");
+			
+		}
+
+	}
+
+}
+//   Accept a number (1–12) from the user and print the name of the month using a switch case.
+package Switch.txt;
+
+public class Checkmon {
+
+	public static void main(String[] args) {
+
+		int num = 1;
+		switch (num) {
+		case 1:
+			System.out.println("Jan");
+			break;
+		case 2:
+			System.out.println("feb");
+			break;
+		case 3:
+			System.out.println("March");
+			break;
+		case 4:
+			System.out.println("April");
+			break;
+		case 5:
+			System.out.println("May");
+			break;
+		case 6:
+			System.out.println("Jun");
+			break;
+		case 7:
+			System.out.println("July");
+			break;
+		case 8:
+			System.out.println("Aug");
+			break;
+		case 9:
+			System.out.println("Sep");
+			break;
+		case 10:
+			System.out.println("Oct");
+			break;
+		case 11:
+			System.out.println("Nov");
+			break;
+		case 12:
+			System.out.println("Dec ");
+			break;
+		default:
+			System.out.println("Enter valid input ");
+		}
+
+	}
+
+}
+//   Accept a number (1–12) from the user and print the name of the month using a switch case.
+package Switch.txt;
+
+public class Checkmon {
+
+	public static void main(String[] args) {
+
+		int num = 1;
+		switch (num) {
+		case 1:
+			System.out.println("Jan");
+			break;
+		case 2:
+			System.out.println("feb");
+			break;
+		case 3:
+			System.out.println("March");
+			break;
+		case 4:
+			System.out.println("April");
+			break;
+		case 5:
+			System.out.println("May");
+			break;
+		case 6:
+			System.out.println("Jun");
+			break;
+		case 7:
+			System.out.println("July");
+			break;
+		case 8:
+			System.out.println("Aug");
+			break;
+		case 9:
+			System.out.println("Sep");
+			break;
+		case 10:
+			System.out.println("Oct");
+			break;
+		case 11:
+			System.out.println("Nov");
+			break;
+		case 12:
+			System.out.println("Dec ");
+			break;
+		default:
+			System.out.println("Enter valid input ");
+		}
+
+	}
+
+}
+////   Accept a number (1–12) from the user and print the name of the month using a switch case.
+package Switch.txt;
+
+public class Checkmon {
+
+	public static void main(String[] args) {
+
+		int num = 1;
+		switch (num) {
+		case 1:
+			System.out.println("Jan");
+			break;
+		case 2:
+			System.out.println("feb");
+			break;
+		case 3:
+			System.out.println("March");
+			break;
+		case 4:
+			System.out.println("April");
+			break;
+		case 5:
+			System.out.println("May");
+			break;
+		case 6:
+			System.out.println("Jun");
+			break;
+		case 7:
+			System.out.println("July");
+			break;
+		case 8:
+			System.out.println("Aug");
+			break;
+		case 9:
+			System.out.println("Sep");
+			break;
+		case 10:
+			System.out.println("Oct");
+			break;
+		case 11:
+			System.out.println("Nov");
+			break;
+		case 12:
+			System.out.println("Dec ");
+			break;
+		default:
+			System.out.println("Enter valid input ");
+		}
+
+	}
+
+}
+//   Accept a number (1–12) from the user and print the name of the month using a switch case.
+package Switch.txt;
+
+public class Checkmon {
+
+	public static void main(String[] args) {
+
+		int num = 1;
+		switch (num) {
+		case 1:
+			System.out.println("Jan");
+			break;
+		case 2:
+			System.out.println("feb");
+			break;
+		case 3:
+			System.out.println("March");
+			break;
+		case 4:
+			System.out.println("April");
+			break;
+		case 5:
+			System.out.println("May");
+			break;
+		case 6:
+			System.out.println("Jun");
+			break;
+		case 7:
+			System.out.println("July");
+			break;
+		case 8:
+			System.out.println("Aug");
+			break;
+		case 9:
+			System.out.println("Sep");
+			break;
+		case 10:
+			System.out.println("Oct");
+			break;
+		case 11:
+			System.out.println("Nov");
+			break;
+		case 12:
+			System.out.println("Dec ");
+			break;
+		default:
+			System.out.println("Enter valid input ");
+		}
+
+	}
+
+}
+
+
