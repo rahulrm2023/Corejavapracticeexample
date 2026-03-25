03f0a74e01ee6c9c21f838dc254908519fb45295
Rahul Metkari <metkarir9@gmail.com>
2026-03-25 12:47:55 +0530

    Add files via upload

---
+++ method/com/Prameter/EvenNo.java
@@ -0,0 +1,12 @@
+package Prameter;
+
+public class EvenNo {
+	public void show( int num) {
+		if(num %2==0) {
+			System.out.println("Number is Even ");
+		}else {
+			System.out.println("Number is Odd  ");
+		}
+	}
+
+}
---
+++ method/com/Prameter/Fibonacciseric.java
@@ -0,0 +1,15 @@
+package Prameter;
+
+public class Fibonacciseric {
+	public void Display( int a ,int b ) {
+		int c;
+		for (int i = 0; i <= 10; i++) {
+			System.out.print(a + ",");
+			c = a + b;
+			a = b;
+			b = c;
+
+		}
+	}
+
+}
